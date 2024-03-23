9c81058078ae6abb4a286e149023ee7dbbf1d99e
中本 浩太（Kota Nakamoto） <kota.nakamoto@kinto-technologies.com>
2024-03-23 11:11:46 +0900

    add ckeditor5-video package and export as plugin

--- admin/src/components/Input/CKEditor/plugins/index.js
+++ admin/src/components/Input/CKEditor/plugins/index.js
@@ -1,2 +1,6 @@
 export { default as StrapiMediaLib } from './StrapiMediaLib';
 export { default as StrapiUploadAdapter } from './StrapiUploadAdapter';
+export { default as Video } from "@visao/ckeditor5-video/src/video";
+import { default as VideoResize } from "@visao/ckeditor5-video/src/videoresize";
+import { default as VideoStyle } from "@visao/ckeditor5-video/src/videostyle";
+import { default as VideoToolbar } from "@visao/ckeditor5-video/src/videotoolbar";
